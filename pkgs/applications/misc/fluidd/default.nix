aba8ce83b75343b51da22797709a6f3741976987
Zhaofeng Li <hello@zhaofeng.li>
2021-07-26 20:06:02 -0700

    fluidd: 1.16.2 -> unstable-2021-07-18

--- pkgs/applications/misc/fluidd/default.nix
+++ pkgs/applications/misc/fluidd/default.nix
@@ -1,28 +1,44 @@
-{ lib, stdenvNoCC, fetchurl, unzip }:
+{ lib, pkgs, stdenvNoCC, fetchFromGitHub, nodejs, nodePackages }:
 
-stdenvNoCC.mkDerivation rec {
-  pname = "fluidd";
-  version = "1.16.2";
+let
+  rev = "c01a473bbaede2a980dd94a3f7f7cc9ceeecc339";
 
-  src = fetchurl {
-    name = "fluidd-v${version}.zip";
-    url = "https://github.com/cadriel/fluidd/releases/download/v${version}/fluidd.zip";
-    sha256 = "1qwj25xvvxvm1fxx216nn2gp7js4d682mm3l4s7ns90fc5ygvc8i";
+  src = fetchFromGitHub {
+    owner = "zhaofengli";
+    repo = "fluidd";
+    inherit rev;
+    sha256 = "0rwbc6h8gqc9gs6rzgw7h1y6z0087zwb5yqb0xmzapnp39h58nhq";
   };
 
-  nativeBuildInputs = [ unzip ];
+  # vue.config.js attempts to run `git rev-parse --short HEAD`
+  # sorry about this :/
+  fakegit = pkgs.writeScriptBin "git" ''
+    #!${pkgs.runtimeShell}
+    echo ${builtins.substring 0 7 rev}
+  '';
+
+  buildDeps = nodePackages.fetchNodeModules {
+    inherit src;
+    makeTarball = false;
+    production = false;
+    sha256 = "12lj3k4msiw4bpdx5i1n70jaylcgrhj65nri298d4zjj3wr7r4d1";
+  };
+in stdenvNoCC.mkDerivation rec {
+  pname = "fluidd";
+  version = "unstable-2021-07-18";
+
+  inherit src;
 
   dontConfigure = true;
-  dontBuild = true;
 
-  unpackPhase = ''
-    mkdir fluidd
-    unzip $src -d fluidd
+  buildPhase = ''
+    ln -s ${buildDeps}/lib/node_modules node_modules
+    PATH=${fakegit}/bin:$PATH ${nodejs}/bin/node node_modules/.bin/vue-cli-service build
   '';
 
   installPhase = ''
-    mkdir -p $out/share/fluidd
-    cp -r fluidd $out/share/fluidd/htdocs
+    mkdir -p $out/share/fluidd/htdocs
+    cp -r dist/* $out/share/fluidd/htdocs
   '';
 
   meta = with lib; {
