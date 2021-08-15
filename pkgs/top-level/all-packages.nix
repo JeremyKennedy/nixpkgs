e46cdd0e2e29c7e7be4e45654452f910173d9f59
Zhaofeng Li <hello@zhaofeng.li>
2021-08-15 12:35:51 -0700

    dafny: init at 3.2.0
    The old dotnetPackages.Dafny is left alone.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -33351,7 +33351,7 @@ with pkgs;
 
   ### SCIENCE/PROGRAMMING
 
-  dafny = dotnetPackages.Dafny;
+  dafny = callPackage ../applications/science/programming/dafny { };
 
   groove = callPackage ../applications/science/programming/groove { };
 
