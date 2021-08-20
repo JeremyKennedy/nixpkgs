8fb39ea4eabe808feb323d864b8d270b241b5dbe
Pierre Bourdon <delroth@gmail.com>
2021-08-20 10:38:42 -0700

    parsec-gaming: init at 2021-01-12

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2374,6 +2374,8 @@ with pkgs;
 
   parquet-tools = callPackage ../tools/misc/parquet-tools { };
 
+  parsec-gaming = callPackage ../applications/misc/parsec-gaming { };
+
   pass = callPackage ../tools/security/pass { };
 
   passphrase2pgp = callPackage ../tools/security/passphrase2pgp { };
