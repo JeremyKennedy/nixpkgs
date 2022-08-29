5221cbebc243aa94ae8a51f48a377449533486ec
Zhaofeng Li <hello@zhaofeng.li>
2022-08-28 19:06:03 -0600

    Revert "phoc: 0.13.0 -> 0.20.0"
    This reverts commit cf4cccece47442fa2c3c75b88a6d121a5af9f95f.

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -9824,7 +9824,9 @@ with pkgs;
 
   pfstools = libsForQt5.callPackage ../tools/graphics/pfstools { };
 
-  phoc = callPackage ../applications/misc/phoc { };
+  phoc = callPackage ../applications/misc/phoc {
+    wlroots = wlroots_0_14;
+  };
 
   phodav = callPackage ../tools/networking/phodav { };
 
