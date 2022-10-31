75165513cd2d1e1e8ea212a8c9a86afdcec6a9e7
Zhaofeng Li <hello@zhaofeng.li>
2022-10-30 18:48:16 -0600

    Revert "cubeb: Add Darwin support"
    This reverts commit 4848a079ee773358b09e320357400e04eefad9de.
    
    Builds but doesn't actually work. Will work on a separate branch.

--- pkgs/development/libraries/audio/cubeb/default.nix
+++ pkgs/development/libraries/audio/cubeb/default.nix
@@ -6,8 +6,6 @@
 , pulseaudio
 , sndio
 , speexdsp
-, CoreAudio
-, CoreServices
 , lazyLoad ? true
 }:
 
@@ -15,15 +13,6 @@ let
   version = "unstable-2022-10-18";
   rev = "27d2a102b0b75d9e49d43bc1ea516233fb87d778";
   hash = "sha256-q+uz1dGU4LdlPogL1nwCR/KuOX4Oy3HhMdA6aJylBRk=";
-
-  backendLibs = if stdenv.isDarwin then [
-    CoreAudio
-  ] else [
-    jack2
-    pulseaudio
-    sndio
-    speexdsp
-  ];
 in stdenv.mkDerivation {
   pname = "cubeb";
   inherit version;
@@ -40,8 +29,12 @@ in stdenv.mkDerivation {
     pkg-config
   ];
 
-  buildInputs = backendLibs
-    ++ lib.optional stdenv.isDarwin CoreServices;
+  buildInputs = [
+    jack2
+    pulseaudio
+    sndio
+    speexdsp
+  ];
 
   cmakeFlags = [
     "-DBUILD_SHARED_LIBS=ON"
@@ -54,14 +47,13 @@ in stdenv.mkDerivation {
 
   passthru = {
     # For downstream users when lazyLoad is true
-    inherit backendLibs;
+    backendLibs = [ jack2 pulseaudio sndio speexdsp ];
   };
 
   meta = with lib; {
     description = "Cross platform audio library";
     homepage = "https://github.com/mozilla/cubeb";
     license = licenses.isc;
-    platforms = platforms.linux ++ platforms.darwin;
     maintainers = with maintainers; [ zhaofengli ];
   };
 }
