77d7df94ed97dca8b14af33f60accaff49b9f0f2
Zhaofeng Li <hello@zhaofeng.li>
2022-11-20 23:52:17 -0700

    bambu-studio: clean up

--- pkgs/applications/misc/prusa-slicer/bambu-studio.nix
+++ pkgs/applications/misc/prusa-slicer/bambu-studio.nix
@@ -1,4 +1,4 @@
-{ lib, fetchFromGitHub, fetchpatch, makeDesktopItem, prusa-slicer
+{ lib, fetchFromGitHub, fetchpatch, prusa-slicer
 , wxGTK31
 , glfw
 , mesa
@@ -14,27 +14,35 @@ let
     ];
   });
 
-  override = super: rec {
-    pname = "bambu-studio";
-    version = "1.3.0.25";
+  versions = {
+    official = rec {
+      version = "1.3.0.25";
 
-    src = fetchFromGitHub {
-      owner = "bambulab";
-      repo = "BambuStudio";
-      rev = "v${version}";
-      hash = "sha256-yaJr3rwkUKS7Ct6qf7irxjoX1ZBoQJLG1H8YKGeRJmY=";
+      src = fetchFromGitHub {
+        owner = "bambulab";
+        repo = "BambuStudio";
+        rev = "v${version}";
+        hash = "sha256-yaJr3rwkUKS7Ct6qf7irxjoX1ZBoQJLG1H8YKGeRJmY=";
+      };
+
+      patches = [
+        # Various fixes to be upstreamed
+        (fetchpatch {
+          url = "https://github.com/zhaofengli/BambuStudio/compare/96707fc4b4b40c30b7e5610d2489ef283fe952a4...897d035d85b5b4e4a9e708551c6b9854c37139f6.patch";
+          hash = "sha256-uQdOjbgny9HBD0ZX52kJLBLeAtotTQBEvtRgW4PT+aY=";
+        })
+
+        # Fix for webkitgtk linking
+        ./0001-not-for-upstream-CMakeLists-Link-against-webkit2gtk-.patch
+      ];
     };
 
-    patches = [
-      # Various fixes to be upstreamed
-      (fetchpatch {
-        url = "https://github.com/zhaofengli/BambuStudio/compare/96707fc4b4b40c30b7e5610d2489ef283fe952a4...897d035d85b5b4e4a9e708551c6b9854c37139f6.patch";
-        hash = "sha256-uQdOjbgny9HBD0ZX52kJLBLeAtotTQBEvtRgW4PT+aY=";
-      })
+    # TODO: SoftFever
+  };
 
-      # Fix for webkitgtk linking
-      ./0001-not-for-upstream-CMakeLists-Link-against-webkit2gtk-.patch
-    ];
+  override = { version, src, patches ? [], ... } @ args: super: rec {
+    pname = "bambu-studio";
+    inherit version src patches;
 
     buildInputs = super.buildInputs ++ [
       glfw
@@ -59,7 +67,17 @@ let
         --run "mkdir -p \"\''${XDG_CONFIG_HOME:-\$HOME/.config}/BambuStudio\""
       )
     '';
-  };
+
+    passthru = allVersions;
+
+    meta = with lib; {
+      description = "PC Software for BambuLab's 3D printers";
+      homepage = "https://github.com/bambulab/BambuStudio";
+      license = licenses.agpl3;
+      maintainers = with maintainers; [ zhaofengli ];
+      mainProgram = "bambu-studio";
+    };
+  } // args;
 
   prusa-slicer' = prusa-slicer.override {
     # We use --run in makeWrapper
@@ -68,4 +86,6 @@ let
     };
     wxGTK31-override = wxGTK31';
   };
-in prusa-slicer'.overrideAttrs override
+
+  allVersions = builtins.mapAttrs (_name: version: (prusa-slicer'.overrideAttrs (override version))) versions;
+in allVersions.official
