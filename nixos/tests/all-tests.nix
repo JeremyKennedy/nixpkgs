6957e4c64056468da643ec45691b40e581d9af92
Zhaofeng Li <hello@zhaofeng.li>
2023-05-01 17:21:17 -0600

    nixos/tests/darling: init

--- nixos/tests/all-tests.nix
+++ nixos/tests/all-tests.nix
@@ -170,6 +170,7 @@ in {
   cups-pdf = handleTest ./cups-pdf.nix {};
   custom-ca = handleTest ./custom-ca.nix {};
   croc = handleTest ./croc.nix {};
+  darling = handleTest ./darling.nix {};
   deluge = handleTest ./deluge.nix {};
   dendrite = handleTest ./matrix/dendrite.nix {};
   dex-oidc = handleTest ./dex-oidc.nix {};
